5647aaab00121b5ff0100af15ead658ff7fed04b
kol1a_ <koljanmuzyka2108@gmail.com>
2023-12-08 00:22:36 +0200

    first initial commit with starter puck of server and client

---
+++ lab-9_server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO.MemoryMappedFiles;
+using System.Threading;
+
+class Server
+{
+    static void Main()
+    {
+        const string eventCharName = "$MyVerySpecialEventName$";
+        const string eventTerminationName = "$MyVerySpecialEventTerminationName$";
+        const string fileShareName = "$MyVerySpecialFileShareName$";
+
+        EventWaitHandle eventChar = new EventWaitHandle(false, EventResetMode.AutoReset, eventCharName);
+        EventWaitHandle eventTermination = new EventWaitHandle(false, EventResetMode.AutoReset, eventTerminationName);
+
+        using (MemoryMappedFile fileMapping = MemoryMappedFile.CreateOrOpen(fileShareName, 100, MemoryMappedFileAccess.ReadWrite))
+        {
+            using (MemoryMappedViewAccessor accessor = fileMapping.CreateViewAccessor())
+            {
+                while (true)
+                {
+                    int waitResult = WaitHandle.WaitAny(new WaitHandle[] { eventTermination, eventChar });
+                    if (waitResult == 0)
+                        break;
+
+                    char character = (char)accessor.ReadByte(0);
+                    Console.Write(character);
+                }
+            }
+        }
+    }
+}
